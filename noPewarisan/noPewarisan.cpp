f8b577477faf6f26184867a59a0e4ad17de4582a
lilalr <gurennut@gmail.com>
2024-05-28 19:05:06 +0700

    class abstraksiKlas

--- noPewarisan/noPewarisan.cpp
+++ noPewarisan/noPewarisan.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 class baseClass {
 public:
-    virtual void perkenalan() {
+    virtual void perkenalan() final {
         cout << "Hallo saya function dari base class";
     }
 };
 
 class derivedClass : public baseClass {
 public:
-    void perkenalan() {
-        cout << "Hallo saya Function dari derived class";
-    }
+    // void perkenalan() {
+    //    cout << "Hallo saya Function dari derived class";
+    //}
 };
 
 int main() {
