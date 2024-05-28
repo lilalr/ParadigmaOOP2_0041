5031e441eaa09c9b0d991c11f231a9c436da197f
lilalr <gurennut@gmail.com>
2024-05-28 18:44:14 +0700

    final untuk mencegah penurunan

--- noOverriding/noOverriding.cpp
+++ noOverriding/noOverriding.cpp
@@ -3,21 +3,5 @@ using namespace std;
 
 class baseClass {
 public:
-    virtual void perkenalan() {
-        cout << "Hallo saya function dari base class";
-    }
-};
-
-class derivedClass : public baseClass {
-public:
-    void perkenalan() {
-        cout << "Hallo saya Function dari derived class";
-    }
-};
-
-int main() {
-    derivedClass a;
-    a.perkenalan();
-
-    return 0;
-}
+    virtual void perkenalan() final {
+        
