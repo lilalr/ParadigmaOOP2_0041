dbe6c73844a2f38da0c26e7b91ecd2f537314ac8
lilalr <gurennut@gmail.com>
2024-05-28 17:41:27 +0700

    membuat library standar

--- polimorpi/polimorpi.cpp
+++ polimorpi/polimorpi.cpp
@@ -1,30 +1,3 @@
 #include <iostream>
 using namespace std;
 
-class seseorang {
-public:
-    virtual void pesan() = 0;
-    //virtual void pesan(){
-    //      cout << "Pesan dari seseorang" << endl;
-    //}
-};
-
-class joko :public seseorang {
-public:
-    void pesan() {
-        cout << "Pesan dari joko" << endl;
-    }
-};
-
-class lia :public seseorang {
-public:
-    void pesan() {
-        cout << "Pesan dari lia" << endl;
-    }
-};
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
-
